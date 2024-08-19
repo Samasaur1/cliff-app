8c922f2df6f1a7e274764e6d44f05457452330ea
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-08-18 18:23:11 -0700

    Initial commit

---
+++ Cliff/CliffApp.swift
@@ -0,0 +1,17 @@
+//
+//  CliffApp.swift
+//  Cliff
+//
+//  Created by Sam Gauck on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CliffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
