3485cd05cd311b21db0f6c8936b6faf9d484d75f
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-08-25 12:07:57 -0700

    Show notification status, fix permission request on macOS
    Also bump version to 0.2.0

--- Cliff/ContentView.swift
+++ Cliff/ContentView.swift
@@ -11,6 +11,7 @@ import UserNotifications
 
 struct ContentView: View {
     @State var serverURL: String = ""
+    @State var authorizationStatus: UNAuthorizationStatus? = nil
     
     var body: some View {
         VStack {
@@ -18,21 +19,47 @@ struct ContentView: View {
             Button {
                 let url = URL(string: serverURL)
                 UserDefaults.standard.set(url, forKey: "server")
-                // register for notifications
-                UNUserNotificationCenter.current()
-                    .requestAuthorization(options: [.alert, .badge, .sound, .carPlay, .criticalAlert]) { granted, error in
-                        print("Permission granted: \(granted)")
-                    }
-                #if os(macOS)
-                NSApplication.shared.registerForRemoteNotifications()
-                #else
-                UIApplication.shared.registerForRemoteNotifications()
-                #endif
+                
+                Task {
+                    // Register for notifications
+                    let granted = try await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound, .carPlay])
+                    print("Permission granted: \(granted)")
+                    #if os(macOS)
+                    NSApplication.shared.registerForRemoteNotifications()
+                    #else
+                    UIApplication.shared.registerForRemoteNotifications()
+                    #endif
+                    self.authorizationStatus = await UNUserNotificationCenter.current().notificationSettings().authorizationStatus
+                }
             } label: {
                 Text("Register for notifications")
             }
+            HStack {
+                Text("Current status:")
+                switch authorizationStatus {
+                case .notDetermined:
+                    Image(systemName: "questionmark")
+                case .denied:
+                    Image(systemName: "xmark")
+                case .authorized:
+                    Image(systemName: "checkmark")
+                case .provisional:
+                    Text("provisional")
+                case .ephemeral:
+                    Text("ephemeral")
+                case nil:
+                    ProgressView()
+                @unknown default:
+                    Text("New case")
+                }
+            }
         }
         .padding()
+        .onAppear {
+            Task {
+                self.authorizationStatus = await UNUserNotificationCenter.current().notificationSettings().authorizationStatus
+            }
+        }
     }
 }
 
